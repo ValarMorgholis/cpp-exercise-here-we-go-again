a1c6df84658af5e4567291b9ce5772bca2313acc
ValarMorghulis <mohamadmahdimirzaieii@gmail.com>
2022-12-06 17:06:03 +0330

    test1

---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 0;
+    char tar[2];
+
+    cin >> tar[0] >> tar[1];
+
+    i = tar[0];
+
+    cout << i << endl;
+    cout << tar[1] << endl;
+    
+
+    return 0;
+}
