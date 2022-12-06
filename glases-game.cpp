1e2a852bd7bb47f380fd6154d82ae5d7bb0d1f09
ValarMorghulis <mohamadmahdimirzaieii@gmail.com>
2022-12-06 16:28:52 +0330

    program work witout problem

--- glases-game.cpp
+++ glases-game.cpp
@@ -5,30 +5,23 @@ using namespace std;
 int main()
 {
     int i = 0;
-    char tar[1], temp[2], temp1[2], temp2[1], result[2] = {0};
+    char tar[1], temp[2], temp1[2], result[2] = {0};
 
-    // cout << "Do your move : ";
+    cout << "Do your move : ";
     cin >> i;
     cin >> tar[0];
 
-    // for (; i < 0;)
-    // {
-    cin >> temp[0] >> temp1[1];
-    // i--;
+    while (i > 0)
+    {
+        cin >> temp[0] >> temp1[1];
 
-    if (temp1[1] == tar[0] && temp1[1] == 'r')
-        result[0] = temp[0];
-
-    else if (temp1[1] == tar[0] && temp1[1] == 'm')
-        result[0] = temp[0];
-
-    else if (temp1[1] == tar[0] && temp1[1] == 'l')
-        result[0] = temp[0];
-
-    // ---------------------------------------------------
-
- 
-    // }
+        if (temp1[1] == tar[0])
+        {
+            result[0] = temp[0];
+            tar[0] = temp[0];
+        }
+        i--;
+    }
     cout << result[0] << endl;
 
     return 0;
